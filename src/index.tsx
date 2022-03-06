b36ff0b5676228413f0a2cb560e2213feb9000b5
Ankit Chouhan <ankitchouhan1020@gmail.com>
2022-03-06 18:29:21 +0530

    init: initial project setup

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+} from 'react';
+
+const createStateContext = <Value, State>(
+  useValue: (init?: Value) => State
+) => {
+  const StateContext = createContext<State | null>(null);
+  const StateProvider = ({
+    initialValue,
+    children,
+  }: {
+    initialValue?: Value;
+    children?: ReactNode;
+  }) => (
+    <StateContext.Provider value={useValue(initialValue)}>
+      {children}
+    </StateContext.Provider>
+  );
+  const useContextState = () => {
+    const value = useContext(StateContext);
+    if (value === null) throw new Error("Provider missing");
+    return value;
+  };
+  return [StateProvider, useContextState] as const;
+};
+
+export { createStateContext };
